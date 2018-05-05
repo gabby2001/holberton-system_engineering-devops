102d60d4183632edf8f80d9d30c48c06819a902a
Yunju Chen <info@yunjuchen.net>
2018-05-05 01:09:16 +0000

    Add headers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     '''Query number of subscribers of a subreddit'''
-    user_agent = {'User-agent': 'Chrome/66.0.3359.139 Safari/537.36'}
+    header = {'User-agent': 'Chrome/66.0.3359.139 Safari/537.36'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    r = requests.get(url)
+    r = requests.get(url, headers=header)
     if r.status_code == 200:
         print(r.json().get('data').get('subscribers'))
     else:
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,9 +5,9 @@ import requests
 
 def top_ten(subreddit):
     '''Print frist 10 posts of a subreddit'''
-    user_agent = {'User-agent': 'Chrome/66.0.3359.139 Safari/537.36'}
+    header = {'User-agent': 'Chrome/66.0.3359.139 Safari/537.36'}
     url = 'https://www.reddit.com/r/{}/new.json?limit=10'.format(subreddit)
-    r = requests.get(url)
+    r = requests.get(url, headers=header)
     if r.status_code == 200:
         for item in r.json().get('data').get('children'):
             print(item.get('data').get('title'))
