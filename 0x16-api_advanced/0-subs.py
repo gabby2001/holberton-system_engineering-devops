c4e7d830799a5e746fc306b8da2f952c384f8815
Yunju Chen <info@yunjuchen.net>
2018-05-05 01:15:11 +0000

    Return result

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,6 +9,6 @@ def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     r = requests.get(url, headers=header)
     if r.status_code == 200:
-        print(r.json().get('data').get('subscribers'))
+        return r.json().get('data').get('subscribers')
     else:
         return 0
